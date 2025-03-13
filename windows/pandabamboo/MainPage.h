17697c8166cf4f889a01636c4b252af091bca051
Mako Adam Peter <makoadampeter@gmail.com>
2025-03-13 21:33:35 +0100

    feat: Create empty project

---
+++ windows/pandabamboo/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::pandabamboo::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::pandabamboo::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
